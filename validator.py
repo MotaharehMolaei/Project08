deacd2924d358dbd192fd636f5bd25267c81273d
Motahareh Molaei <motahareh_molaei@yahoo.de>
2025-05-29 15:52:55 +0200

    Changed

--- validator.py
+++ validator.py
@@ -1,32 +1,28 @@
 import re
 
-class Validator:
-    def __init__(self, address, house_date):
-        self.address = address
-        self.house_date = house_date
-
-    def is_valid_address(self):
-        return bool(re.match(r'^[a-zA-Zآ-ی0-9\s\-,]{10,100}$', self.address))
+def address_validator(address):
+    return re.match(r'^[a-zA-Zآ-ی0-9\s\-,]{10,100}$', address)
+    # return bool(re.match(r'^[\w\s\d]{10,100}$', address))
 
 # (id[0], address[1], region[2], has_elevator[3], has_parking[4], has_storage[5], rooms[6])
-    def validate(self):
-        errors = []
+def house_validator(house):
+
+    errors = []
 
     # Validate ID
-        if not (type(self.house[0]) == int and self.house[0] > 0):
-            errors.append("House ID must be a positive integer.")
+    if not (type(house[0]) == int and house[0] > 0):
+        errors.append("House ID must be a positive integer.")
 
     # Validate Address
-        if not address_validator(self.house[1]):
-            errors.append("Address is invalid.")
+    if not address_validator(house[1]):
+        errors.append("Address is invalid.")
 
     # Validate Region
-        if not (type(self.house[2]) == str and re.match(r"^[a-zA-Zآ-ی0-9\s\-,]{3,50}$", self.house[2])):
-            errors.append("Region name is invalid. English and persian letters and spaces are allowed.")
+    if not (type(house[2]) == str and re.match(r"^[a-zA-Zآ-ی0-9\s\-,]{3,50}$", house[2])):
+        errors.append("Region name is invalid. English and persian letters and spaces are allowed.")
 
     # Validate Room count
-        if not (type(self.house[6]) == int and self.house[6] > 0):
-            errors.append("Room count must be a positive integer.")
+    if not (type(house[6]) == int and house[6] > 0):
+        errors.append("Room count must be a positive integer.")
 
-        return errors
-
+    return errors
